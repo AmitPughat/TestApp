c4a5d1a21c383bfa92774cd99282a99031925c15
amititconsultant@hotmail.com <amititconsultant@hotmail.com>
2018-04-30 22:26:14 -0500

    1

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -19,7 +19,7 @@ namespace TestApp
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                //.UseUrls("http://*:100")
+                .UseUrls("http://*:100")
                 .UseStartup<Startup>()
                 .Build();
     }
